dd1a0acd10cfb5e05910f1eaf40f3a580a43a3d1
Vatsal Srivastava <113903916+Vatsal9915@users.noreply.github.com>
2025-10-31 13:14:27 +0530

    Add 'use client' directive to page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Navigation from "@/components/navigation"
 import PackagingTransition from "@/components/packaging-transition"
 import ProductsGrid from "@/components/products-grid"
