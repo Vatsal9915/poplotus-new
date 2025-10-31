773c1a108968e0a51ddfcde43bc66ce44db4b0bd
Vatsal Srivastava <113903916+Vatsal9915@users.noreply.github.com>
2025-10-31 13:59:28 +0530

    Update page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import Navigation from "@/components/navigation"
 import PackagingTransition from "@/components/packaging-transition"
 import ProductsGrid from "@/components/products-grid"
